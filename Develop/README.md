84ce7292b1d3b1f3d2f497d75690d83d68d58970
Mark <mark.speer11@gmail.com>
2020-08-25 12:27:18 -0400

    commit

---
+++ Develop/README.md
@@ -0,0 +1,53 @@
+
+# **no title**
+
+N/A
+
+## Description 
+
+a simple project
+
+## Table of contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+
+
+## Installation
+
+        install ......
+
+## Usage
+
+.......
+
+## Licence
+
+free
+
+## Contributors
+
+just me
+
+## Test
+
+.....
+
+
+## Repository
+
+- [Project Repo]()
+
+## GitHub
+
+![My image](https://avatars3.githubusercontent.com/u/66832524?v=4)
+- Mark Speer
+- [My GitHub Profile](https://github.com/mark-speer)
+- <null>
+
---
+++ Develop/index.js
@@ -0,0 +1,88 @@
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you wish to add."
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Please provide your licence or your badge link"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide who have contributed"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the tests for your project"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is the link to your repo?"
+    },
+];
+
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file creation successful!");
+          });
+        });
+
+});
+
+function init() {
+
+}
+
+init();
